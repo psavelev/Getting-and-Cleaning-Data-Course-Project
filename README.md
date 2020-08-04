6b3bdd73acdfeee681d5dc433d2035f5c8671ee7
Pavel S <psavelev@gmail.com>
2020-08-04 18:13:31 +0200

    Initial implementation of run_analysis.R script, README and CookBook

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# Getting-and-Cleaning-Data-Course-Project
-Getting and Cleaning Data Course Project
+# Getting and Cleaning Data Course Project
+
+This is the final Getting an Cleaning Data Course project.
+
+The data preparation script is implemented in **run_analysis.R**, which does the following:
+
+1. Downloads dataset from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and unpack it
+2. Reads "dictionary" data: list of activities (activity_labels.txt) and list of features (features.txt)
+3. Creates vectors with features and their approprate names which contaits 'mean' and 'std'. They will be used to select the only data whcih containts mean and standard deviations from the datasets.
+4. Reads Testing and training datasets, mearges botth datasets to one. Appropriately labels the data set with descriptive variable names are used. Descriptive activity names are set in the data set
+5. Creates independent tidy data set with the average of each variable for each activity and each subject.
+6. Saves tidy dataset to FUCI_HAR_Dataset_Tidy.csv (which is submitted to Coursera)
+
+The original dataset was published by:
+
+        Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
+        Smartlab - Non Linear Complex Systems Laboratory
+        DITEN - Universit? degli Studi di Genova.
+        Via Opera Pia 11A, I-16145, Genoa, Italy.
+        activityrecognition@smartlab.ws
+        www.smartlab.ws
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,74 @@
+library(dplyr)
+library(tidyr)
+
+# Data initialization
+urlDataset <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+nameFileArchive <- "FUCI_HAR_Dataset.zip"
+nameDataDirectory <- "data"
+nameDataUCIDirectory <- paste(nameDataDirectory, "UCI\ HAR\ Dataset", sep = "/")
+nameFileTidyDataset <- paste(nameDataDirectory, "FUCI_HAR_Dataset_Tidy.csv", sep = "/")
+
+# Reads specified datasets, bind data to one dataframe
+# Parameters:
+#       nameFileData - relative file path to the file with data set
+#       nameFileActivities - relative file path to the file with labels data
+#       nameFileSubjects - relative file path to the file with data related to subject 
+#                       who performed the activity (IDs in range from 1 to 30)
+#       columnsList - columns to be selected from nameFileAll dataset
+#       columnsNames - the names of columns which are specified by columnsList vector
+# Returns dataset
+readData <- function (nameFileData, nameFileActivities, nameFileSubjects, 
+                     columnsList, columnsNames, dtActivitiesList) {
+        
+        # Read  dataset
+        dtData <- read.table(paste(nameDataUCIDirectory, nameFileData, sep = "/"))
+        dtActivities <- read.table(paste(nameDataUCIDirectory, 
+                                         nameFileActivities, sep = "/"))
+        dtActivities$ActivityName = as.factor(dtActivitiesList$Activity[dtActivities[,1]])
+        dtSubjects <- read.table(paste(nameDataUCIDirectory, 
+                                       nameFileSubjects, sep = "/"))
+        dt <- cbind(dtActivities$ActivityName, dtSubjects, dtData[, columnsList])
+        colnames(dt) <- c("Activity", "Subject", columnsNames)
+        
+        dt
+}
+
+# Prepare dataset
+
+# Check if archived dataset file is not existing in work directory, then download it
+if (!file.exists(nameFileArchive)) {
+        download.file(urlDataset, nameFileArchive)
+}
+# Unzip dataset from archive, overwrite existing files
+unzip(nameFileArchive, exdir = nameDataDirectory)
+
+# Read "dictionary" data
+dtActivities <- read.table(paste(nameDataUCIDirectory, "activity_labels.txt", 
+                               sep = "/"), stringsAsFactors = FALSE)
+colnames(dtActivities) <- c("Code", "Activity")
+dtFeatures <- read.table(paste(nameDataUCIDirectory, "features.txt", 
+                              sep = "/"), stringsAsFactors = FALSE)
+colnames(dtFeatures) <- c("Code", "Feature")
+
+# Get vector of features containing mean and std (Standard Distribution) values
+features <- grep(".(mean|std).", dtFeatures$Feature)
+# Get names of the feature and make values suitable (remove -, () chars) for column naming
+featureNames <- dtFeatures[features, "Feature"]
+
+# Read and merge datasets
+dtTraining <- readData("train/X_train.txt", "train/Y_train.txt", 
+                       "train/subject_train.txt", features, featureNames,
+                       dtActivities)
+dtTesting <- readData("test/X_test.txt", "test/y_test.txt", 
+                      "test/subject_test.txt", features, featureNames,
+                      dtActivities)
+dtAll <- as_tibble(rbind(dtTraining, dtTesting))
+rm(dtTraining, dtTesting)
+
+# Create Tidy dataset with the average of each variable for each activity and each subject.
+dtTidy <- dtAll %>% 
+        gather(key = "Variable", value = "Value", -Activity:-Subject) %>% 
+        group_by(Activity, Subject, Variable) %>% 
+        summarise(mean(Value))
+
+write.csv(dtTidy, file = nameFileTidyDataset)
